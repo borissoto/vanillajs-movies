54dcb8bfe27d4493758931a427c4bb60e16f90f5
Borrison <borissoto@gmail.com>
2021-01-21 22:46:26 -0400

    extracting utility functions

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: '',
+      apikey: '193b44a8',
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  return response.data.Search; // it is uppercase S beacuse of the api, in general you should expect lowercase letter
 };
 
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 1000);
-};
+const onInput = debounce((event) => {
+  const movies = fetchData(event.target.value);
 
-input.addEventListener('input', onInput);
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}"/>
+      <h1>${movie.Titel}</h1>
+    `;
+  }
+});
+
+input.addEventListener('input', debounce(onInput, 500));

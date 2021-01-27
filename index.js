61dda121c7265cbe9211014b49058fff6cfea658
Borrison <borissoto@gmail.com>
2021-01-26 23:50:51 -0400

    making autocomplete reusable

--- index.js
+++ index.js
@@ -13,57 +13,14 @@ const fetchData = async (searchTerm) => {
   return response.data.Search; // it is uppercase S beacuse of the api, in general you should expect lowercase letter
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search For a Movie</b></label>
-  <input class = "input" />
-  <div class = "dropdown ">
-    <div class = "dropdown-menu">
-      <div class = "dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-      <img src="${imgSrc}" />
-      ${movie.Title}
-    `;
-
-    option.addEventListener('click', () => {
-      dropdown.classList.remove('is-active');
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    });
-
-    resultsWrapper.appendChild(option);
-  }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-  }
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+});
+createAutoComplete({
+  root: document.querySelector('.autocomplete-two'),
+});
+createAutoComplete({
+  root: document.querySelector('.autocomplete-three'),
 });
 
 const onMovieSelect = async (movie) => {
@@ -91,6 +48,26 @@ const movieTemplate = (movieDetail) => {
           <h4>${movieDetail.Genre}</h4>
           <p>${movieDetail.Plot}</p>
       </div>
-    </article>    
+    </article> 
+    <article class="notification is-primary">   
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">   
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">   
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">   
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">imdbRating</p>
+    </article>
+    <article class="notification is-primary">   
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">imdbVotes</p>
+    </article>
   `;
 };
